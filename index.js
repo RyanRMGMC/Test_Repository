99e0cc12aa2fff83ad1fe4423cc18f9252d5453b
Ryan Couto <ryanrafael2003@gmail.com>
2024-01-02 10:44:17 -0300

    Um terceiro contribuiu no projeto

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ import {turboLogger} from './turboLogger.js'
 console.log('Estou usando o GitHub');
 
 turboLogger('Estou aprendendo o fluxo do git', 'green');
+turboLogger('Contribuição de terceiros para o projeto', 'pink');
