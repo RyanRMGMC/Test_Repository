d266501b701d2a69f01dc406f2a70df7f7676de5
Ryan Couto <ryanrafael2003@gmail.com>
2024-01-02 10:29:22 -0300

    Atualizei uma nova brench

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ import {turboLogger} from './turboLogger.js'
 console.log('Estou usando o GitHub');
 
 turboLogger('Estou aprendendo o fluxo do git', 'green');
+turboLogger('Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema!', 'yellow');
